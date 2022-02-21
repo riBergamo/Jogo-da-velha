c7563a375a15d374bcaeb27d1084d73ed719c272
Riane Bergamo <89801847+riBergamo@users.noreply.github.com>
2022-02-21 14:37:22 -0300

    Update TesteJogoDaVelha.java

--- Jodo-da-Velha/src/br/com/jogoDaVelha/Testes/TesteJogoDaVelha.java
+++ Jodo-da-Velha/src/br/com/jogoDaVelha/Testes/TesteJogoDaVelha.java
@@ -5,8 +5,6 @@ import br.com.jogoDaVelha.Modelos.Jogo;
 public class TesteJogoDaVelha {
     public static void main(String[] args) {
         Jogo jogo = new Jogo();
-        //prolema: Da empate quando a ultima jogada decide a vitoria(REOSLVIDO)
-        //criar uma exception
 
     }
 }
